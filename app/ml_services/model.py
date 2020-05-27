c74022e7bb89079ddbb3064fff3b3e6bdea12122
JWilson1172 <jw59615@gmail.com>
2020-05-27 16:41:10 -0400

    NN pair programing

---
+++ app/ml_services/model.py
@@ -0,0 +1,14 @@
+# app/ml_services/model.py
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+class MLModel():
+
+    def __init__(self):
+        return None
+
+    def fit(self, user_input, prefrences):
+
+        pass
